7f3328f79f0cc78ac47229aaf58e90d59fed5d36
rajdeepdaspwc <rajdeep.das@pwc.com>
2021-08-17 19:19:18 +0530

    Current Model size and AR UI change

--- ar/app.js
+++ ar/app.js
@@ -336,14 +336,14 @@ class App {
     }
 
     onZoomIn = () => {
-      this.scale = math.round(this.scale) + math.round(this.scale / 10);
+      this.scale = math.round(this.scale) + (math.round(this.scale) / 10);
     	current_object.scale.set(this.scale, this.scale, this.scale);
       //$("#currentModelSize span").text("-");
       $("#currentModelSize span").text((this.scale.toFixed(2) * 100));
     }
 
     onZoomOut = () => {
-    	this.scale = math.round(this.scale) + math.round(this.scale / 10);
+    	this.scale = math.round(this.scale) - (math.round(this.scale) / 10);
     	current_object.scale.set(this.scale, this.scale, this.scale);
       $("#currentModelSize span").text((this.scale.toFixed(2) * 100));
     }
